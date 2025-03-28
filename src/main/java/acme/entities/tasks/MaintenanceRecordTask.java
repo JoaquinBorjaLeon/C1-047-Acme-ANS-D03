169a166d999c54fb88955821b33e649b975d061f
DLJ7666 <jarguezduran@gmail.com>
2025-03-28 16:30:05 +0100

    uncommented esential code

--- src/main/java/acme/entities/tasks/MaintenanceRecordTask.java
+++ src/main/java/acme/entities/tasks/MaintenanceRecordTask.java
@@ -3,7 +3,9 @@ package acme.entities.tasks;
 
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
+import javax.validation.Valid;
 
+import acme.entities.maintenancerecord.MaintenanceRecord;
 import acme.client.components.basis.AbstractEntity;
 import acme.client.components.mappings.Automapped;
 import acme.client.components.validation.Mandatory;
@@ -16,12 +18,11 @@ import lombok.Setter;
 public class MaintenanceRecordTask extends AbstractEntity {
 
 	private static final long	serialVersionUID	= 1L;
-
-//	TO-DO: uncomment this when/if MaintenanceRecord class is accepted
-//	@ManyToOne
-//	@Mandatory
-//	@Automapped
-//	private MaintenanceRecord MaintenanceRecord;
+	
+	@ManyToOne
+	@Mandatory
+	@Automapped
+	private MaintenanceRecord MaintenanceRecord;
 	
 	@ManyToOne
 	@Mandatory
