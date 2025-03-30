ca541fc0053e2028dc16d53ce39f9e87933a5713
DLJ7666 <jarguezduran@gmail.com>
2025-03-30 18:03:48 +0200

    creación repositorio de mrs para tecnicos

---
+++ src/main/java/acme/features/maintenancerecord/TechnicianMaintenanceRecordRepository.java
@@ -0,0 +1,20 @@
+
+package acme.features.maintenancerecord;
+
+import java.util.Collection;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import acme.client.repositories.AbstractRepository;
+import acme.entities.maintenancerecord.MaintenanceRecord;
+
+@Repository
+public interface TechnicianMaintenanceRecordRepository extends AbstractRepository {
+
+	@Query("SELECT m FROM MaintenanceRecord m WHERE m.technician.id = :technicianId ")
+	Collection<MaintenanceRecord> findAllByTechnicianId(final int technicianId);
+
+	@Query("SELECT m FROM MaintenanceRecord m WHERE m.id = :id")
+	MaintenanceRecord findById(int id);
+}
