9f2f4ac6d94a82d0b8e92348508e412fa4d41ad6
DLJ7666 <jarguezduran@gmail.com>
2025-03-30 04:26:25 +0200

    Cambio al orden de validación estándar

--- src/main/java/acme/entities/tasks/MaintenanceRecordTask.java
+++ src/main/java/acme/entities/tasks/MaintenanceRecordTask.java
@@ -7,7 +7,6 @@ import javax.validation.Valid;
 
 import acme.entities.maintenancerecord.MaintenanceRecord;
 import acme.client.components.basis.AbstractEntity;
-import acme.client.components.mappings.Automapped;
 import acme.client.components.validation.Mandatory;
 import lombok.Getter;
 import lombok.Setter;
@@ -19,14 +18,14 @@ public class MaintenanceRecordTask extends AbstractEntity {
 
 	private static final long	serialVersionUID	= 1L;
 	
-	@ManyToOne
 	@Mandatory
-	@Automapped
+	@Valid
+	@ManyToOne
 	private MaintenanceRecord MaintenanceRecord;
 	
-	@ManyToOne
 	@Mandatory
-	@Automapped
+	@Valid
+	@ManyToOne
 	private Task task;
 	
 }
