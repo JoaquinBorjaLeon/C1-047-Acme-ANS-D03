6274f500f2f86df449df247678cb335b33f5df0b
DLJ7666 <jarguezduran@gmail.com>
2025-04-01 01:05:54 +0200

    Servicio de creación de resguardos de mantenimiento para técnicos

---
+++ src/main/java/acme/features/maintenancerecord/TechnicianMaintenanceRecordCreateService.java
@@ -0,0 +1,97 @@
+package acme.features.maintenancerecord;
+
+import java.util.Collection;
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import acme.client.components.models.Dataset;
+import acme.client.components.views.SelectChoices;
+import acme.client.helpers.MomentHelper;
+import acme.client.services.AbstractGuiService;
+import acme.client.services.GuiService;
+import acme.entities.aircraft.Aircraft;
+import acme.entities.maintenancerecord.MaintenanceRecord;
+import acme.entities.maintenancerecord.MaintenanceRecordStatus;
+import acme.realms.technician.Technician;
+
+@GuiService
+public class TechnicianMaintenanceRecordCreateService extends AbstractGuiService<Technician, MaintenanceRecord> {
+
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	private TechnicianMaintenanceRecordRepository repository;
+
+
+	// AbstractGuiService interface -------------------------------------------
+	@Override
+	public void authorise() {
+		super.getResponse().setAuthorised(true);
+	}
+
+	@Override
+	public void load() {
+		MaintenanceRecord maintenanceRecord;
+		Date moment = MomentHelper.getCurrentMoment();
+		Technician technician = (Technician) super.getRequest().getPrincipal().getActiveRealm();
+
+		maintenanceRecord = new MaintenanceRecord();
+		maintenanceRecord.setTechnician(technician);
+		maintenanceRecord.setMaintenanceMoment(moment);
+		super.getBuffer().addData(maintenanceRecord);
+	}
+
+	@Override
+	public void bind(final MaintenanceRecord maintenanceRecord) {
+		super.bindObject(maintenanceRecord, "status", "nextInspectionDate", "estimatedCost", "notes", "aircraft");
+	}
+
+	@Override
+	public void validate(final MaintenanceRecord maintenanceRecord) {
+
+		if (!this.getBuffer().getErrors().hasErrors("status"))
+			super.state(maintenanceRecord.getStatus() != null, "status", "acme.validation.maintenancerecord.status.message", maintenanceRecord);
+
+		if (!this.getBuffer().getErrors().hasErrors("nextInspectionDate") && maintenanceRecord.getNextInspectionDate() != null)
+			super.state(maintenanceRecord.getNextInspectionDate().after(maintenanceRecord.getMaintenanceMoment()), 
+					"nextInspectionDate", "acme.validation.maintenancerecord.nextinspectiondate.message", maintenanceRecord);
+
+		if (!this.getBuffer().getErrors().hasErrors("estimatedCost") && maintenanceRecord.getEstimatedCost() != null)
+			super.state(0.00 <= maintenanceRecord.getEstimatedCost().getAmount() && maintenanceRecord.getEstimatedCost().getAmount() <= 1000000.00, 
+					"estimatedCost", "acme.validation.maintenancerecord.estimatedCost.message", maintenanceRecord);
+
+		if (!this.getBuffer().getErrors().hasErrors("notes") && maintenanceRecord.getNotes() != null)
+			super.state(maintenanceRecord.getNotes().length() <= 255, "notes", "acme.validation.maintenancerecord.notes.message", maintenanceRecord);
+
+		}
+
+	@Override
+	public void perform(final MaintenanceRecord maintenanceRecord) {
+		assert maintenanceRecord != null;
+
+		this.repository.save(maintenanceRecord);
+	}
+
+	@Override
+	public void unbind(final MaintenanceRecord maintenanceRecord) {
+		SelectChoices choices;
+		Collection<Aircraft> aircrafts;
+		SelectChoices aircraft;
+
+		Dataset dataset;
+		aircrafts = this.repository.findAllAircrafts();
+		choices = SelectChoices.from(MaintenanceRecordStatus.class, maintenanceRecord.getStatus());
+		aircraft = SelectChoices.from(aircrafts, "id", maintenanceRecord.getAircraft());
+
+		dataset = super.unbindObject(maintenanceRecord, "status", "nextInspectionDate", "estimatedCost", "notes", "aircraft");
+
+		dataset.put("status", choices.getSelected().getKey());
+		dataset.put("status", choices);
+		dataset.put("aircraft", aircraft.getSelected().getKey());
+		dataset.put("aircraft", aircraft);
+
+		super.getResponse().addData(dataset);
+	}
+
+}
